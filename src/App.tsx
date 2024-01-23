e96c2eef226f0e14952dfd0bfaa962d3e07fc970
tuvngdevsl <tuvngdevsl@gmail.com>
2024-01-24 00:03:17 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+   <div> Hello world
+     <h1> Hihi</h1>     <h2>ab</h2>
+   </div> 
+  )
+}
+
+export default App
